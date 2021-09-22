57056c268de346557449590a5eace794dfe03fb7
Muhammad Yaasin <muhammadyaasin@ZCTJKTN2100001.local>
2021-09-22 11:17:54 +0700

    first test

---
+++ function.js
@@ -0,0 +1,22 @@
+const { set } = require("lodash");
+
+function sum(a, b) {
+    return a+b;
+}
+
+console.log(sum(10, 10)); //20
+
+function product(a, b){
+    return a*b;
+}
+
+console.log(product(12, 12)); 
+
+const bigProduct = product(234, 345);
+console.log(bigProduct);
+
+setTimeout(function () {
+    console.log("hey you rocks !");
+
+}, 1000);
+
---
+++ object.js
@@ -0,0 +1,11 @@
+const cookie = {
+
+name: "chocolate chip",
+isGlutenFree: false,
+
+};
+
+cookie.name = "chocolate chips";
+cookie.isGlutenFree = true;
+console.log(cookie["+yummy"]); 
+console.log(cookie);   
