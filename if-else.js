16da354e27c4ab2503bc539875096dd485c8d67b
Yaasin04 <muhammad.yaasin@zebrax.id>
2021-09-22 16:42:19 +0700

    first commit

---
+++ if-else.js
@@ -0,0 +1,11 @@
+let attendance = 16;
+
+if (attendance < 15) {
+    console.log("we're full capacity");
+}
+else if (attendance > 15){
+    console.log("we're over capacity");
+}
+else {
+    console.log("we are full capacity");
+}
