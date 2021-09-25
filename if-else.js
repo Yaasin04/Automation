65355d6451cca2517fcac3e3262ec4cbe62473f3
Yaasin04 <muhammad.yaasin@zebrax.id>
2021-09-25 22:33:05 +0700

    ada perubahan

---
+++ if-else.js
@@ -0,0 +1,13 @@
+let attendance = 16;
+
+if (attendance < 15) {
+    console.log("we're full capacity");
+}
+else if (attendance > 15){
+    console.log("we're over capacity");
+}
+else {
+    console.log("we are full capacity");
+    console.log("stop here");
+
+}   
